13153f6abfd7fd9258c7b57a5855a7a01b834b5c
antonio <antoniobelmardacosta@gmail.com>
2017-11-07 11:30:29 +0000

    Adds views directory with index file

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get "/secret" do
 end
 
 get "/cat" do
-  '<img style="border: medium dashed red" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" /img>'
+  erb(:index)
 end
 
 get "/:name" do |name|
